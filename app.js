9313a7d260e5bcc75f5e11e51bc8d23700777787
Leo Bonjo <139697327+LeoBonjo@users.noreply.github.com>
2024-09-27 11:24:41 -0400

    update google maps dependency

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 
+// shows that the backend is deployed successfully
 app.get("/", (req, res) => {
   res.send("Welcome to the API!");
 });
