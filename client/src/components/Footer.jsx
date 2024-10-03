a9b5650acf4333f49ef8a12da15095712510aa51
Leo Bonjo <139697327+LeoBonjo@users.noreply.github.com>
2024-10-03 18:40:48 -0400

    comment out the map component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,21 +4,22 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <a href="#">
+      <hr></hr>
+      <a href="https://github.com/LeoBonjo/tasteful-traditions">
         <FaGithubSquare
           id="social-icon"
           target="_blank"
           className="h-200 w-12 text-slate-500 hover:text-black duration-300"
         />
       </a>
-      <a href="#">
+      <a href="https://www.linkedin.com/in/leo-bonjo/">
         <FaLinkedin
           id="social-icon"
           target="_blank"
           className="h-8 w-8 text-slate-500 hover:text-black duration-300"
         />
       </a>
-      <a href="#">
+      <a href="https://x.com/?lang=en">
         <FaTwitterSquare
           id="social-icon"
           className="h-8 w-8 text-slate-500 hover:text-black duration-300"
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from React Router
-import Navbar from './Navbar';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; // Import Link from React Router
+import Navbar from "./Navbar";
 import Footer from "./Footer";
-import './Homepage.css';
-import MapContainer from './MapContainer';
+import "./Homepage.css";
+import MapContainer from "./MapContainer";
 
 const Homepage = () => {
   const [dishOfTheDay, setDishOfTheDay] = useState(null);
@@ -22,13 +22,13 @@ const Homepage = () => {
         setDishOfTheDay(dishData);
 
         // Fetch the Featured Recipes
-        const featuredPromises = featuredRecipeIds.map(id =>
-          fetch(`/api/recipes/${id}`).then(response => response.json())
+        const featuredPromises = featuredRecipeIds.map((id) =>
+          fetch(`/api/recipes/${id}`).then((response) => response.json())
         );
         const featuredResults = await Promise.all(featuredPromises);
-        setFeaturedRecipes(featuredResults); 
+        setFeaturedRecipes(featuredResults);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -44,11 +44,18 @@ const Homepage = () => {
             <h2>This Week's Feature</h2>
             {dishOfTheDay ? (
               <div className="dish-of-the-day-content">
-                <img src={dishOfTheDay.image} alt={dishOfTheDay.title} className="dish-image" />
+                <img
+                  src={dishOfTheDay.image}
+                  alt={dishOfTheDay.title}
+                  className="dish-image"
+                />
                 <div className="dish-details">
                   <h3>{dishOfTheDay.title}</h3>
                   <p>{dishOfTheDay.culture_desc}</p>
-                  <Link to={`/recipes/${dishOfTheDay.id}`} className="view-recipe-link">
+                  <Link
+                    to={`/recipes/${dishOfTheDay.id}`}
+                    className="view-recipe-link"
+                  >
                     View the Recipe
                   </Link>
                 </div>
@@ -63,8 +70,12 @@ const Homepage = () => {
           <h2>More to Love</h2>
           <div className="featured-recipes-grid">
             {featuredRecipes.length > 0 ? (
-              featuredRecipes.map(recipe => (
-                <Link key={recipe.id} to={`/recipes/${recipe.id}`} className="recipe-item-link">
+              featuredRecipes.map((recipe) => (
+                <Link
+                  key={recipe.id}
+                  to={`/recipes/${recipe.id}`}
+                  className="recipe-item-link"
+                >
                   <div className="recipe-item">
                     <img src={recipe.image} alt={recipe.title} />
                     <h3>{recipe.title}</h3>
@@ -77,7 +88,7 @@ const Homepage = () => {
           </div>
         </section>
       </div>
-      <MapContainer />
+      {/* <MapContainer /> */}
       <Footer />
     </>
   );
